7ef0a4eae6276701e603258cdee920cf0c615a54
doupl2018-create <doupl.2018@gmail.com>
2026-04-28 09:25:06 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -12,34 +12,11 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
-// معالجة الإشعارات في الخلفية
-messaging.onBackgroundMessage((payload) => {
-  console.log('إشعار في الخلفية:', payload);
-  // مفيش داعي تعمل self.registration.showNotification هنا لو بتبعت من الـ Console
-  // لأن الـ Console بيبعت payload بيخلي المتصفح يعرض الإشعار لوحده
-});
-
-// ده الجزء اللي هيصلح الـ 404 في الإشعار اللي بيتبعت من الكونسول
 self.addEventListener('notificationclick', function(event) {
   event.notification.close();
   
-  // بلاش الرابط الثابت، خليك ذكي وخد الرابط من الإشعار نفسه
-  const urlToOpen = event.notification.data?.FCM_MSG?.notification?.click_action 
-                     || event.notification.data?.click_action
-                     || event.notification.data?.url
-                     || 'https://doupl2018-create.github.io/dh-automation/';
-
+  // الرابط اللي انت عايزه
   event.waitUntil(
-    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function(clientList) {
-      for (let i = 0; i < clientList.length; i++) {
-        let client = clientList[i];
-        if (client.url === urlToOpen && 'focus' in client) {
-          return client.focus();
-        }
-      }
-      if (clients.openWindow) {
-        return clients.openWindow(urlToOpen);
-      }
-    })
+    clients.openWindow('https://doupl2018-create.github.io/dh-automation/')
   );
 });
